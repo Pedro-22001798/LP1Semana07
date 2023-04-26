8c5ef96e410c28b8387f3047c5e6d0537bef241d
Pedro Silva <73141072+Pedro-22001798@users.noreply.github.com>
2023-04-26 09:51:43 +0100

    Added Getters and Setters + GetGrey()

--- FillSpheres/Program.cs
+++ FillSpheres/Program.cs
@@ -22,5 +22,44 @@
             this.blue = newBlue;
             this.alpha = newAlpha;
         }
+
+        public Color(byte newRed, byte newGreen, byte newBlue)
+        {
+            this.red = newRed;
+            this.green = newGreen;
+            this.blue = newBlue;
+            this.alpha = 255;
+        }
+
+        public byte GetRed() => red;
+        public byte GetGreen() => green;
+        public byte GetBlue() => blue;
+        public byte GetAlpha() => alpha;
+
+        public void SetRed(byte newRed)
+        {
+            red = newRed;
+        }
+
+        public void SetGreen(byte newGreen)
+        {
+            green = newGreen;
+        }
+
+        public void SetBlue(byte newBlue)
+        {
+            blue = newBlue;
+        }
+
+        public void SetAlpha(byte newAlpha)
+        {
+            alpha = newAlpha;
+        }
+
+        public byte GetGrey()
+        {
+            int grey = (red + green + blue) / 3;
+            return (byte)grey;
+        }
     }
 }

90afd80e22ed511ce9e04400389ecefdd6cc0bf3
Pedro Silva <73141072+Pedro-22001798@users.noreply.github.com>
2023-04-26 10:31:23 +0100

    Added files from previous project

--- BetterFillSpheres/Program.cs
+++ BetterFillSpheres/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace FillSpheres
+{
+    class Program 
+    {         
+        static void Main(string[] args)
+        {
+            Color red = new Color(255,0,0,255);
+            Sphere sphere1 = new Sphere(red,5f);
+
+            Color blue = new Color(0,0,255,255);
+            Sphere sphere2 = new Sphere(blue,3f);
+
+            Color green = new Color(0,255,0,255);
+            Sphere sphere3 = new Sphere(green,10f);
+
+            sphere1.Pop();
+            sphere2.Throw();
+            sphere3.Throw();
+            sphere3.Throw();
+
+            Console.WriteLine($"Sphere2 was thrown {sphere2.GetTimesThrown()} times and Sphere3 was thrown {sphere3.GetTimesThrown()} times");
+
+        }
+    }
+}

03612d2db8d8d49049cb08dd356dccd2f24be06e
Pedro Silva <73141072+Pedro-22001798@users.noreply.github.com>
2023-04-26 09:43:06 +0100

    Added Color class and constructor

--- FillSpheres/Program.cs
+++ FillSpheres/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace FillSpheres
+{
+    class Program 
+    {         
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Color
+    {
+        private byte red;
+        private byte green;
+        private byte blue;
+        private byte alpha;
+
+        public Color(byte newRed, byte newGreen, byte newBlue, byte newAlpha)
+        {
+            this.red = newRed;
+            this.green = newGreen;
+            this.blue = newBlue;
+            this.alpha = newAlpha;
+        }
+    }
+}

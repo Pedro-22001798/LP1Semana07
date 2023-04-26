51c2bb106d8bf19fe2f62331be52430b6dfe6c04
Pedro Silva <73141072+Pedro-22001798@users.noreply.github.com>
2023-04-26 10:07:15 +0100

    Sphere Class + Getter + Creation of spheres and Colors on main

--- FillSpheres/Program.cs
+++ FillSpheres/Program.cs
@@ -4,7 +4,22 @@
     {         
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World!");
+            Color red = new Color(255,0,0,255);
+            Sphere sphere1 = new Sphere(red,5f);
+
+            Color blue = new Color(0,0,255,255);
+            Sphere sphere2 = new Sphere(blue,3f);
+
+            Color green = new Color(0,255,0,255);
+            Sphere sphere3 = new Sphere(green,10f);
+
+            sphere1.Pop();
+            sphere2.Throw();
+            sphere3.Throw();
+            sphere3.Throw();
+
+            Console.WriteLine($"Sphere2 was thrown {sphere2.GetTimesThrown()} times and Sphere3 was thrown {sphere3.GetTimesThrown()} times");
+            
         }
     }
 }
---
+++ FillSpheres/Sphere.cs
@@ -0,0 +1,44 @@
+namespace FillSpheres
+{
+    class Sphere
+    {
+        private readonly Color color;
+        private float radius;
+        private int numThrow;
+
+        public Sphere(Color newColor, float newRadius)
+        {
+            this.color = newColor;
+            this.radius = newRadius;
+            this.numThrow = 0;
+        }
+
+        public void Pop()
+        {
+            if(radius > 0)
+            {
+                radius = 0f;
+                Console.WriteLine("Boom!");
+            }
+            else
+            {
+                Console.WriteLine("How do you want to pop a popped sphere?");
+            }
+        }
+
+        public void Throw()
+        {
+            if(radius > 0)
+            {
+                numThrow++;
+                Console.WriteLine("Ball thrown");
+            }
+            else
+            {
+                Console.WriteLine("That ball is popped");
+            }
+        }
+
+        public int GetTimesThrown() => numThrow;
+    }
+}

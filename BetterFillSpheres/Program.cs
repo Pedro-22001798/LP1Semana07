052bcec89579208368d9ddd553be8ae2190a2605
Pedro Silva <73141072+Pedro-22001798@users.noreply.github.com>
2023-04-28 10:51:21 +0100

    Player Class Created

--- BetterFillSpheres/Program.cs
+++ BetterFillSpheres/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace FillSpheres
+﻿namespace BetterFillSpheres
 {
     class Program 
     {         
---
+++ PlayerStats/Player.cs
@@ -0,0 +1,63 @@
+using System;
+
+namespace PlayerStats
+{
+    public class Player
+    {
+        private float highScore;
+        private int playedGames;
+        private int wonGames;
+        private readonly string name;
+
+        public Player(string playerName)
+        {
+            this.name = playerName;
+            this.highScore = 0f;
+            this.playedGames = 0;
+            this.wonGames = 0;
+        }
+        public float HighScore
+        {
+            get
+            {
+                return highScore;
+            }
+            set
+            {
+                if(value > highScore)
+                    highScore = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public float WinRate
+        {
+            get
+            {
+                if(playedGames > 0)
+                {
+                    float winRate = wonGames / playedGames;
+                    return winRate;
+                }
+                else
+                    return 0f;
+            }
+        }
+
+        public void PlayGame(bool win)
+        {
+            playedGames++;
+            if(win)
+            {
+                wonGames++;
+            }
+        }
+    }
+}
